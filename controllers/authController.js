28f6037dd4d17f2a6812f456cd50d3bc71b85c0e
vani491 <er94shivanisingh@gmail.com>
2025-06-17 14:55:10 +0530

    Initial commit

---
+++ controllers/authController.js
@@ -0,0 +1,25 @@
+const User = require('../models/User');
+
+exports.register = async (req, res) => {
+  const { name, email, password } = req.body;
+
+  try {
+    const user = new User({ name, email, password });
+    await user.save();
+    res.status(201).json({ message: 'User registered', user });
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
+
+exports.login = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email, password });
+    if (!user) return res.status(401).json({ message: 'Invalid credentials' });
+    res.status(200).json({ message: 'Login successful', user });
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
